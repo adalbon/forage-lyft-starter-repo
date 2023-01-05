ca805927841afd75c2660cfd1e34de728e206a26
Alessandro Dal Bon <alessandro.dalbon@duke.edu>
2023-01-05 17:56:04 -0500

    implemented tires

--- car.py
+++ car.py
@@ -1,10 +1,12 @@
 from Engine import Engine
 from Battery import Battery
+from tires.tire import Tire
 
-class Car(Engine, Battery):
-    def __init__(self, engine, battery):
+class Car(Engine, Battery, Tire):
+    def __init__(self, engine, battery, tires):
         self.engine = engine
         self.battery = battery
+        self.tires = tires
 
     def battery_needs_service(self):
         return self.battery.battery_needs_service()
@@ -12,5 +14,8 @@ class Car(Engine, Battery):
     def engine_needs_service(self):
         return self.engine.engine_needs_service()
 
+    def tire_needs_service(self):
+        return self.tires.tires_needs_service()
+
     def needs_service(self):
-        return self.engine_needs_service() or self.battery_needs_service()
+        return self.engine_needs_service() or self.battery_needs_service() or self.tire_needs_service()
